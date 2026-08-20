7df6087a0f3fbbff556a22b592bda4452824a61c
Morpheus <167074500+TheGittyPerson@users.noreply.github.com>
2026-08-20 17:13:44 +0900

    Add number guessing game

---
+++ games/random/number_guessing_game/game.py
@@ -0,0 +1,36 @@
+import random
+
+AUTHOR = "TheGittyPerson"
+
+
+def run():
+    print(f"Game by {AUTHOR}")
+
+    number = random.randint(1, 100)
+    tries = 6
+    print(f"\nTry to guess the number in {tries} tries.")
+    while tries > 0:
+        if not (inp := input("\nEnter a number 1~100: ")).isdigit():
+            print("\nInvalid input: Only enter digits. Try again.")
+
+        inp = int(inp)
+
+        if inp < 1 or inp > 100:
+            print("\nInvalid input: Only enter numbers from 1 to 100. Try "
+                  "again.")
+            continue
+
+        if inp == number:
+            print(f"\nCorrect! The number is indeed {number}.")
+            return
+
+        if inp < number:
+            print("\nToo low! Try again.")
+        if inp > number:
+            print("\nToo high! Try again.")
+
+        tries -= 1
+        print(f"Remaining tries: {tries}")
+
+    print("\nYou ran out of tries :(")
+    print(f"The number was {number}.")
